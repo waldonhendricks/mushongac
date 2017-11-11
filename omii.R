ee44c08274c4c4e4f8154ac628a7cbc9f28cb34e
Developer_Mld <Developer_Mld>
2017-11-11 09:00:16 +0200

    Student 214275124 ICE assignmemnt

--- omii.R
+++ omii.R
@@ -1,3 +1,16 @@
-Reccesess<-c(1,4)
-c
-
+library(xlsx)
+library(rJava)
+library(xlsx)
+report<-"C:\\Users\\User\\Documents\\reportTable.xlsx"
+myReport<-read.xlsx(report,1)
+myReport
+summary(myReport)
+hist(myReport$Term5,xlab="percentage per subject",ylab="value",col="Blue",main="Term5 results")
+boxplot(x=myReport ,data="Term3",varwidth = TRUE,notch=FALSE)
+library(ggplot2)
+library(reshape2)
+reportTable=melt(myReport)
+reportTable
+colnames(reportTable)=c("Subject","Term","Percentage")
+reportTable
+ggplot(reportTable,aes(x="Subject",y="Percentage"))+geom_point(aes(shape="Percentage"))
